1e9ea7bf760fee967207c42b91faeba21f88c25e
Reid Hansell <reidhansell@gmail.com>
2023-07-28 11:35:27 -0600

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+const cron = require('node-cron');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+const channelId = '1088842972558270485';
+
+client.login(token).catch(error => {
+    console.error(`Failed to login: ${error}`);
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    const locations = ['Bestine', 'Daeric', 'Keren'];
+
+    for (let i = 0; i < 8; i++) {
+        const location = locations[i % 3];
+        cron.schedule(`45 ${((i * 2) + 4) % 24} * * *`, () => {
+            const channel = client.channels.cache.get(channelId);
+            if (!channel) {
+                console.error(`Channel with ID ${channelId} not found!`);
+                return;
+            }
+            channel.send(`${location} invasion in 15 minutes!`)
+                .catch(error => console.error(`Failed to send message: ${error}`));
+        });
+    }
+});
